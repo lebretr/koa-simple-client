966a93c35b130a4d747c085d91d2c91301a8f2b4
Rémi Lebret <7931261+lebretr@users.noreply.github.com>
2019-06-25 12:47:11 +0200

    eslint

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/*eslint no-console: 1*/
+'use strict';
+
 var fs = require('fs');
 var path = require('path');
 var http = require('http');
@@ -16,57 +19,58 @@ app.use(async ctx=>{
         host: ctx.host,
         url: ctx.url,
         method: ctx.method,
-        header: ctx.header,
+        headers: ctx.headers,
         type: ctx.type,
         body: ctx.request.body
     };
     console.log(JSON.stringify(ctx.body));
-})
+});
 
 var config = {
-  domain: 'localhost',
-  http: {
-    port: 8000,
-  },
-  https: {
-    port: 8443,
-    options: {
-      key: fs.readFileSync(path.resolve(process.cwd(), 'certs/key.pem'), 'utf8'),
-      cert: fs.readFileSync(path.resolve(process.cwd(), 'certs/cert.pem'), 'utf8')
+    domain: 'localhost',
+    http: {
+        port: 8000,
     },
-  },
+    https: {
+        port: 8443,
+        options: {
+            key: fs.readFileSync(path.resolve(process.cwd(), 'certs/key.pem'), 'utf8'),
+            cert: fs.readFileSync(path.resolve(process.cwd(), 'certs/cert.pem'), 'utf8'),
+            ca: fs.readFileSync(path.resolve(process.cwd(), 'certs/ca/minica.pem'), 'utf8')
+        },
+    },
 };
 
 let serverCallback = app.callback();
 try {
-  var httpServer = http.createServer(serverCallback);
-  httpServer
-    .listen(config.http.port, function(err) {
-      if (!!err) {
-        console.error('HTTP server FAIL: ', err, (err && err.stack));
-      }
-      else {
-        console.log(`HTTP  server OK: http://${config.domain}:${config.http.port}`);
-      }
-    });
+    var httpServer = http.createServer(serverCallback);
+    httpServer
+        .listen(config.http.port, function(err) {
+            if (err) {
+                console.error('HTTP server FAIL: ', err, (err && err.stack));
+            }
+            else {
+                console.log(`HTTP  server OK: http://${config.domain}:${config.http.port}`);
+            }
+        });
 }
 catch (ex) {
-  console.error('Failed to start HTTP server\n', ex, (ex && ex.stack));
+    console.error('Failed to start HTTP server\n', ex, (ex && ex.stack));
 }
 try {
-  var httpsServer = https.createServer(config.https.options, serverCallback);
-  httpsServer
-    .listen(config.https.port, function(err) {
-      if (!!err) {
-        console.error('HTTPS server FAIL: ', err, (err && err.stack));
-      }
-      else {
-        console.log(`HTTPS server OK: https://${config.domain}:${config.https.port}`);
-      }
-    });
+    var httpsServer = https.createServer(config.https.options, serverCallback);
+    httpsServer
+        .listen(config.https.port, function(err) {
+            if (err) {
+                console.error('HTTPS server FAIL: ', err, (err && err.stack));
+            }
+            else {
+                console.log(`HTTPS server OK: https://${config.domain}:${config.https.port}`);
+            }
+        });
 }
 catch (ex) {
-  console.error('Failed to start HTTPS server\n', ex, (ex && ex.stack));
+    console.error('Failed to start HTTPS server\n', ex, (ex && ex.stack));
 }
 
-module.exports = app;
+// module.exports = app;
